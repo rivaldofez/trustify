2e84cb0cc071b8cce4a11743b760d67471bb6039
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-04-15 10:22:21 +0700

    [FEAT] Setup initialization project main app

---
+++ trustify-mainapp/Trustify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trustify
+//
+//  Created by Rivaldo Fernandes on 15/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
---
+++ trustify-mainapp/TrustifyUITests/TrustifyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TrustifyUITestsLaunchTests.swift
+//  TrustifyUITests
+//
+//  Created by Rivaldo Fernandes on 15/04/25.
+//
+
+import XCTest
+
+final class TrustifyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
